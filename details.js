7b809575be2610744a64546e1b7031d0195d943a
LeoLakshman <70107094+LeoLakshman@users.noreply.github.com>
2025-03-02 22:51:29 -0600

    Update details.js

--- details.js
+++ details.js
@@ -1,13 +1,19 @@
 const studentDetailsDiv = document.getElementById('studentDetails');
-const cityStudents = JSON.parse(localStorage.getItem('cityStudents'));
+const cityName = localStorage.getItem('cityName');
 
-if (cityStudents && cityStudents.length > 0) {
-    let detailsHTML = `<h2>Students from ${cityStudents[0].city}</h2>`;
-    cityStudents.forEach(student => {
-        detailsHTML += `<p>Major: ${student.major || 'N/A'}</p>`;
-        detailsHTML += `<p>Details: ${student.otherDetails || 'N/A'}</p>`;
-    });
-    studentDetailsDiv.innerHTML = detailsHTML;
-} else {
-    studentDetailsDiv.innerHTML = `<p>No student details available.</p>`;
-}
+fetch('https://gist.githubusercontent.com/LeoLakshman/2da0383115823f0c3f0395678899d666/raw/d771fb4249a66078a2ea00eda39d3809d2a231c0/studentData.json')
+.then(res => res.json())
+.then(studentData =>{
+    const cityStudents = studentData.filter(student => student.city === cityName);
+
+    if (cityStudents && cityStudents.length > 0) {
+        let detailsHTML = `<h2>Students from ${cityName}</h2>`;
+        cityStudents.forEach(student => {
+            detailsHTML += `<p>Major: ${student.major || 'N/A'}</p>`;
+            detailsHTML += `<p>Details: ${student.otherDetails || 'N/A'}</p>`;
+        });
+        studentDetailsDiv.innerHTML = detailsHTML;
+    } else {
+        studentDetailsDiv.innerHTML = `<p>No student details available.</p>`;
+    }
+});
